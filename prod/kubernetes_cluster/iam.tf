89fbfbd7a714cb69571ce8c1049605d4ec12a66f
José Hernández <jonandez91@gmail.com>
2021-08-20 13:43:11 -0300

    IAM modifications

--- prod/kubernetes_cluster/iam.tf
+++ prod/kubernetes_cluster/iam.tf
@@ -43,34 +43,29 @@ resource "aws_iam_policy" "eks_full_access" {
 })
 }
 # Attache policy to Jenkins server role 
-resource "aws_iam_policy_attachment" "attachemt_eks_policy" {
-  name       = "attachemt_eks_policy"
-  roles      = [aws_iam_role.nodes.name]
+resource "aws_iam_role_policy_attachment" "attachment_eks_policy" {
+  role      = aws_iam_role.nodes.name
   policy_arn = aws_iam_policy.eks_full_access.arn
 }
 
 // temas de interfaces de red y cosas a nivel de plugins
-resource "aws_iam_policy_attachment" "attachemt1" {
-  name       = "attachmen1"
-  roles      = [aws_iam_role.nodes.name]
+resource "aws_iam_role_policy_attachment" "attachemt1" {
+  role      = aws_iam_role.nodes.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 // permisos al register
-resource "aws_iam_policy_attachment" "attachemt2" {
-  name       = "attachmen2"
-  roles      = [aws_iam_role.nodes.name]
+resource "aws_iam_role_policy_attachment" "attachemt2" {
+  role      = aws_iam_role.nodes.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_policy_attachment" "attachemt3" {
-  name       = "attachmen3"
-  roles      = [aws_iam_role.nodes.name]
+resource "aws_iam_role_policy_attachment" "attachemt3" {
+  role      = aws_iam_role.nodes.name
   policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
 }
 
-resource "aws_iam_policy_attachment" "attachemt4" {
-  name       = "attachmen4"
-  roles      = [aws_iam_role.nodes.name]
+resource "aws_iam_role_policy_attachment" "attachemt4" {
+  role      = aws_iam_role.nodes.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
